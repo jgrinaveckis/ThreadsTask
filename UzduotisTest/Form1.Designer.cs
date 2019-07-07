cdc3597492297d983edbd95c94be5837a1dc2a1e
Justas Grinaveckis <grinaveckisjustas@gmail.com>
2019-07-07 20:35:50 +0300

    Add project files.

---
+++ UzduotisTest/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace UzduotisTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.Start = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.Stop = new System.Windows.Forms.Button();
+            this.Clear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Location = new System.Drawing.Point(30, 83);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(400, 314);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.ListView1_SelectedIndexChanged);
+            // 
+            // Start
+            // 
+            this.Start.Location = new System.Drawing.Point(206, 31);
+            this.Start.Name = "Start";
+            this.Start.Size = new System.Drawing.Size(75, 23);
+            this.Start.TabIndex = 1;
+            this.Start.Text = "Start";
+            this.Start.UseVisualStyleBackColor = true;
+            this.Start.Click += new System.EventHandler(this.Start_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(47, 30);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 24);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // Stop
+            // 
+            this.Stop.Location = new System.Drawing.Point(646, 31);
+            this.Stop.Name = "Stop";
+            this.Stop.Size = new System.Drawing.Size(75, 23);
+            this.Stop.TabIndex = 3;
+            this.Stop.Text = "Stop";
+            this.Stop.UseVisualStyleBackColor = true;
+            this.Stop.Click += new System.EventHandler(this.Stop_Click);
+            // 
+            // Clear
+            // 
+            this.Clear.Location = new System.Drawing.Point(451, 30);
+            this.Clear.Name = "Clear";
+            this.Clear.Size = new System.Drawing.Size(75, 23);
+            this.Clear.TabIndex = 4;
+            this.Clear.Text = "Clear";
+            this.Clear.UseVisualStyleBackColor = true;
+            this.Clear.Click += new System.EventHandler(this.Clear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Clear);
+            this.Controls.Add(this.Stop);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.Start);
+            this.Controls.Add(this.listView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Button Start;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button Stop;
+        private System.Windows.Forms.Button Clear;
+    }
+}
+
---
+++ UzduotisTest/Form1.cs
@@ -0,0 +1,237 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace UzduotisTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            ComboBoxPopulation();
+            listView1.View = View.Details;
+            listView1.Columns.Add("ThreadID", 200);
+            listView1.Columns.Add("Data", 200);
+            listView1.Width = 400;
+            listView1.GridLines = true;
+            ClearTable();
+        }
+
+        string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;
+                                    AttachDbFilename=C:\Users\grina\Desktop\UzduotisTest\UzduotisTest\UzduotisTest\Database1.mdf;
+                                    Integrated Security=True";
+        private bool stopWasPressed = false;
+
+        private void Start_Click(object sender, EventArgs e)
+        {
+            int number = 0;
+            try
+            {
+                number = int.Parse(comboBox1.SelectedItem.ToString());
+            }
+            catch
+            {
+                MessageBox.Show("Nepasirinktas threadu skaicius!");
+            }
+
+            //nesukuriant kito threado "luzta" UI.
+            var backThread = new Thread(() =>
+            {
+                BackgroundTask(number);
+                
+            });
+            backThread.Start();
+        }
+
+        private void Stop_Click(object sender, EventArgs e)
+        {
+            stopWasPressed = true;
+
+        }
+
+        //Generuojant random info naudojam static random objekta
+        //kad Next() butu kvieciamas tam paciam Random, o ne skirtingiems Random tuo paciu metu
+        public static readonly Random random = new Random();
+        private static readonly object syncLock = new object();
+        public string GenerateString()
+        {
+            int index = 0;
+            string valid = "1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            lock (syncLock)
+            {
+                index = random.Next(5, 10);
+            }
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < index; i++)
+            {
+                sb.Append(valid[random.Next(valid.Length)]);
+            }
+            Thread.Sleep(random.Next(500, 2000));
+            return sb.ToString();
+        }
+
+        public void AddingInfoToListView(object sender, EventArgs e)
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                SqlDataAdapter adap = new SqlDataAdapter("select * from Duomenys", conn);
+                DataTable dt = new DataTable();
+                adap.Fill(dt);
+                //ziurim ar yra 20 sugeneruotu eiluciu
+                //jei >20 - parasom viska
+                //jei <20 - tik paskutines 20 eiluciu
+                if(dt.Rows.Count - 20 < 0)
+                {
+                    for (int i = 0; i < dt.Rows.Count; i++)
+                    {
+                        DataRow dr = dt.Rows[i];
+                        string[] row = { dr["ThreadID"].ToString(), dr["Data"].ToString() };
+                        ListViewItem listViewItem = new ListViewItem(row);
+                        if(listView1.InvokeRequired)
+                        {
+                            listView1.Invoke(new MethodInvoker(delegate
+                            {
+                                listView1.Items.Add(listViewItem);
+                            }));
+                        }
+                        else
+                        {
+                            listView1.Items.Add(listViewItem);
+                        }
+                        
+
+                    }
+                }
+                else
+                {
+                    for (int i = dt.Rows.Count - 20; i < dt.Rows.Count; i++)
+                    {
+                        DataRow dr = dt.Rows[i];
+                        string[] row = { dr["ThreadID"].ToString(), dr["Data"].ToString() };
+                        ListViewItem listViewItem = new ListViewItem(row);
+                        if (listView1.InvokeRequired)
+                        {
+                            listView1.Invoke(new MethodInvoker(delegate
+                            {
+                                listView1.Items.Add(listViewItem);
+                            }));
+                        }
+                        else
+                        {
+                            listView1.Items.Add(listViewItem);
+                        }
+                    }
+                }
+            }
+        }
+
+        public void AddingInfoToTable()
+        {
+            while(!stopWasPressed)
+            {
+                string data = GenerateString();
+                int threadId = int.Parse(Thread.CurrentThread.Name);
+                DateTime time = DateTime.Now;
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+                    using (SqlTransaction tran = conn.BeginTransaction())
+                    {
+                        using (SqlCommand comm = conn.CreateCommand())
+                        {
+                            comm.Transaction = tran;
+                            comm.CommandType = CommandType.Text;
+                            comm.CommandText = "INSERT INTO Duomenys (ThreadID, Time, Data) values (@threadId, @time, @data)";
+                            comm.Parameters.Add(new SqlParameter("@threadID", threadId));
+                            comm.Parameters.Add(new SqlParameter("@Time", time));
+                            comm.Parameters.Add(new SqlParameter("@Data", data));
+
+                            try
+                            {
+                                if (comm.ExecuteNonQuery() != 1)
+                                {
+                                    throw new InvalidProgramException();
+                                }
+                                tran.Commit();
+                            }
+                            catch (Exception)
+                            {
+                                tran.Rollback();
+                                conn.Close();
+                                throw;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        public void ComboBoxPopulation()
+        {
+            for (int i = 2; i <= 15; i++)
+            {
+                comboBox1.Items.Add(i.ToString());
+            }
+        }
+
+        //isvalom db paleidziant
+        public void ClearTable()
+        {
+            string sqlTrunc = "TRUNCATE TABLE Duomenys";
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand(sqlTrunc, conn);
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        private void BackgroundTask(int num)
+        {
+            
+            List<Thread> threads = new List<Thread>();
+            while (!stopWasPressed)
+            {
+                threads.Clear();
+                for (int i = 1; i <= num; i++)
+                {
+                    Thread thread = new Thread(new ThreadStart(AddingInfoToTable));
+                    thread.Name = i.ToString();
+                    thread.Start();
+                    threads.Add(thread);
+                }
+
+                foreach (Thread thread in threads)
+                {
+                    thread.Join();
+                }
+            }
+        }
+
+        private void ListView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Reikia antra karta paspausti Start buttona, jog rodytu data 
+            Start.Click +=  new EventHandler(AddingInfoToListView);
+        }
+
+        private void Clear_Click(object sender, EventArgs e)
+        {
+            listView1.Items.Clear();
+        }
+    }
+}
